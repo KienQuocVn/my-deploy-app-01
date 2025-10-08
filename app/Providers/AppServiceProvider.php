362bca3e9ba6e61cddac024ca51fccd24556d450
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2025-10-08 16:13:09 +0700

    update and build static

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (request()->isSecure() || request()->header('X-Forwarded-Proto') === 'https') {
-            URL::foreScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
